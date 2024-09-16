837dd8d042e9fa14642bde7ebb789835d41be2f3
Francisco Alberca <francisco.albercac@gmail.com>
2024-09-16 15:30:50 -0500

    Create Operations-Room resource w/ getAll()

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { StaffModule } from './staff/staff.module';
+import { OperationsRoomModule } from './operations-room/operations-room.module';
+
 
 
 @Module({
-  imports: [AuthModule, UserModule, StaffModule],
+  imports: [AuthModule, UserModule, StaffModule, OperationsRoomModule],
   controllers: [],
   providers: [PrismaService],
 })
