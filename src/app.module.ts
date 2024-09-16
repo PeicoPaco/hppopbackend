23db654aa37bc239bc7499d450b65ee57ad15afe
Francisco Alberca <francisco.albercac@gmail.com>
2024-09-16 15:08:06 -0500

    Create Roles resource w/ getAll path

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { StaffModule } from './staff/staff.module';
+import { RolesModule } from './roles/roles.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule, StaffModule],
+  imports: [AuthModule, UserModule, StaffModule, RolesModule],
   controllers: [],
   providers: [PrismaService],
 })
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { RolesService } from './roles.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/auth/enums/rol.enum';
 
 @Controller('roles')
+@UseGuards(AuthGuard, RolesGuard)
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   @Get()
   findAll() {
     return this.rolesService.findAll();
