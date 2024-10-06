230d493617de7837d2b002d1155efb522646d47c
Francisco Alberca <francisco.albercac@gmail.com>
2024-10-06 13:54:49 -0500

    Add Roles guard to operation.controller

--- src/operation/operation.controller.ts
+++ src/operation/operation.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, UseGuards } from '@nestjs/common';
 import { OperationService } from './operation.service';
 import { CreateOperationDto } from './dto/create-operation.dto';
 import { UpdateOperationDto } from './dto/update-operation.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { Role } from 'src/auth/enums/rol.enum';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('operation')
+@UseGuards(AuthGuard, RolesGuard)
 export class OperationController {
   constructor(private readonly operationService: OperationService) {}
 
@@ -12,16 +17,19 @@ export class OperationController {
     return this.operationService.create(createOperationDto);
   }
 
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   @Get()
   findAll() {
     return this.operationService.findAll();
   }
 
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.operationService.findOne(id);
   }
 
+  @Roles(Role.SUPERADMIN, Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateOperationDto: UpdateOperationDto) {
     return this.operationService.update(id, updateOperationDto);
